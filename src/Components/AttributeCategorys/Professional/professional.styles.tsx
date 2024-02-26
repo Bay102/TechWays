061fa80a28b8a4534c5194139eb41a0f2d63e499
Tyson <deppravacion@gmail.com>
2024-02-25 18:21:22 -0700

    style layout changes

--- src/Components/AttributeCategorys/Professional/professional.styles.tsx
+++ src/Components/AttributeCategorys/Professional/professional.styles.tsx
@@ -1,11 +1,12 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const ProfessionalContainer = styled.div`
   display: flex;
+  flex: 2;
   flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: center;
-  width: 45%;
+  justify-content: center;
+  align-items: start;
+  /* width: 45%; */
   height: 400px;
   border: 1px solid red;
-`
+`;
