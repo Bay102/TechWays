5df544ee6d6f38eaf2d77a321dba3543410c19a2
Zak Bay <zakbay@MacBook-Air.local>
2024-02-26 19:53:53 -0800

    rendering lists

--- src/Components/AttributeCategorys/Professional/Professional.tsx
+++ src/Components/AttributeCategorys/Professional/Professional.tsx
@@ -11,8 +11,8 @@ export const Professional = observer(({ attributeStore }: Props) => {
   return (
     <S.ProfessionalContainer>
       <AttributeCard title="Paths" attributes={[]} />
-      <AttributeCard title="Industry" attributes={[]} />
-      <AttributeCard title="Goals" attributes={[]} />
+      <AttributeCard title="Industry" attributes={attributeStore.industry} />
+      <AttributeCard title="Goals" attributes={attributeStore.goals} />
       <AttributeCard title="Market Difficulty" attributes={[]} />
       <AttributeCard title="Skill Level" attributes={[]} />
     </S.ProfessionalContainer>
