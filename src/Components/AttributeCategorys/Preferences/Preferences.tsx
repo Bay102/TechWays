2d1945bce6a0765240f4af887b92dc225d6cd869
Tyson <deppravacion@gmail.com>
2024-02-28 21:49:23 -0700

    add updated paths to educationChoices

--- src/Components/AttributeCategorys/Preferences/Preferences.tsx
+++ src/Components/AttributeCategorys/Preferences/Preferences.tsx
@@ -1,20 +1,35 @@
-import { observer } from 'mobx-react'
-import * as S from './preferences.styles'
-import { AttributeStoreProps } from '../AttributesStore'
-import AttributeCard from '../../AttributeCard/AttributeCard'
+import { observer } from "mobx-react";
+import * as S from "./preferences.styles";
+import { AttributeStoreProps } from "../AttributesStore";
+import AttributeCard from "../../AttributeCard/AttributeCard";
 
 type Props = {
-  attributeStore: AttributeStoreProps
-}
+  attributeStore: AttributeStoreProps;
+};
 
 export const Preferences = observer(({ attributeStore }: Props) => {
   return (
     <S.PreferencesContainer>
-      <AttributeCard title="Length" attributes={[]} />
-      <AttributeCard title="In-Person/Remote" attributes={[]} />
-      <AttributeCard title="Learning Format" attributes={[]} />
-      <AttributeCard title="Community" attributes={[]} />
-      <AttributeCard title="Mentorship" attributes={[]} />
+      <AttributeCard
+        title='Length'
+        attributes={attributeStore.educationalChoices.commnity}
+      />
+      <AttributeCard
+        title='In-Person/Remote'
+        attributes={attributeStore.educationalChoices.learningFormat}
+      />
+      <AttributeCard
+        title='Learning Format'
+        attributes={attributeStore.educationalChoices.lengthOfTime}
+      />
+      <AttributeCard
+        title='Community'
+        attributes={attributeStore.educationalChoices.mentorship}
+      />
+      <AttributeCard
+        title='Mentorship'
+        attributes={attributeStore.educationalChoices.shoolSetting}
+      />
     </S.PreferencesContainer>
-  )
-})
+  );
+});
