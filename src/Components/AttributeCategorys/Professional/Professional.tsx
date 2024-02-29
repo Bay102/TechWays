638febf8c6f71c14544dd625bd0ac92214854e4b
Zak Bay <zakbay@MacBook-Air.local>
2024-02-28 21:04:42 -0800

    bug title

--- src/Components/AttributeCategorys/Professional/Professional.tsx
+++ src/Components/AttributeCategorys/Professional/Professional.tsx
@@ -1,37 +1,35 @@
-import * as S from "./professional.styles";
-import AttributeCard from "../../AttributeCard/AttributeCard";
-import { AttributeStoreProps } from "../AttributesStore";
-import { observer } from "mobx-react";
+import * as S from './professional.styles'
+import AttributeCard from '../../AttributeCard/AttributeCard'
+import { AttributeStoreProps } from '../AttributesStore'
+import { observer } from 'mobx-react'
 
 type Props = {
-  attributeStore: AttributeStoreProps;
-};
+  attributeStore: AttributeStoreProps
+}
 
 export const Professional = observer(({ attributeStore }: Props) => {
   return (
     <S.ProfessionalContainer>
-     
       <AttributeCard
         title="Skill Level"
-        title='Paths'
         attributes={attributeStore.professionalChoices.paths}
       />
       <AttributeCard
-        title='Industry'
+        title="Industry"
         attributes={attributeStore.professionalChoices.goals}
       />
       <AttributeCard
-        title='Goals'
+        title="Goals"
         attributes={attributeStore.professionalChoices.industry}
       />
       <AttributeCard
-        title='Market Difficulty'
+        title="Market Difficulty"
         attributes={attributeStore.professionalChoices.marketDifficulty}
       />
       <AttributeCard
-        title='Skill Level'
+        title="Skill Level"
         attributes={attributeStore.professionalChoices.skillLevel}
       />
     </S.ProfessionalContainer>
-  );
-});
+  )
+})
