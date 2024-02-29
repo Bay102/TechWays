7c94bb9a606cf0fa433c784bba5c7ed903480c72
Zak Bay <zakbay@MacBook-Air.local>
2024-02-28 18:48:29 -0800

    pulling changes

--- src/Components/AttributeCategorys/Professional/Professional.tsx
+++ src/Components/AttributeCategorys/Professional/Professional.tsx
@@ -1,35 +1,40 @@
-import * as S from "./professional.styles";
-import AttributeCard from "../../AttributeCard/AttributeCard";
-import { AttributeStoreProps } from "../AttributesStore";
-import { observer } from "mobx-react";
+import * as S from './professional.styles'
+import AttributeCard from '../../AttributeCard/AttributeCard'
+import { AttributeStoreProps } from '../AttributesStore'
+import { observer } from 'mobx-react'
 
 type Props = {
-  attributeStore: AttributeStoreProps;
-};
+  attributeStore: AttributeStoreProps
+}
 
 export const Professional = observer(({ attributeStore }: Props) => {
   return (
     <S.ProfessionalContainer>
       <AttributeCard
-        title='Paths'
+        title="Paths"
         attributes={attributeStore.professionalChoices.paths}
       />
       <AttributeCard
-        title='Industry'
+        title="Industry"
         attributes={attributeStore.professionalChoices.goals}
       />
       <AttributeCard
-        title='Goals'
+        title="Goals"
         attributes={attributeStore.professionalChoices.industry}
       />
       <AttributeCard
-        title='Market Difficulty'
+        title="Market Difficulty"
         attributes={attributeStore.professionalChoices.marketDifficulty}
       />
       <AttributeCard
-        title='Skill Level'
+        title="Skill Level"
         attributes={attributeStore.professionalChoices.skillLevel}
       />
+      <AttributeCard title="Industry" attributes={attributeStore.industry} />
+      <AttributeCard title="Goals" attributes={attributeStore.goals} />
+      <AttributeCard title="Paths" attributes={[]} />
+      <AttributeCard title="Market Difficulty" attributes={[]} />
+      <AttributeCard title="Skill Level" attributes={[]} />
     </S.ProfessionalContainer>
-  );
-});
+  )
+})
