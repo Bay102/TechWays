5cafb34b3eac3b1264ee93531d82ef377ecb05a6
Tyson <deppravacion@gmail.com>
2024-03-24 18:26:02 -0600

    make it react

--- src/DaisyComponents/Accordian.tsx
+++ src/DaisyComponents/Accordian.tsx
@@ -1,23 +1,29 @@
-<div className='join join-vertical w-full'>
-  <div className='collapse collapse-arrow join-item border border-base-300'>
-    <input type='radio' name='my-accordion-4' defaultChecked />
-    <div className='collapse-title text-xl font-medium'>Interests</div>
-    <div className='collapse-content'>
-      <p>hello</p>
-    </div>
-  </div>
-  <div className='collapse collapse-arrow join-item border border-base-300'>
-    <input type='radio' name='my-accordion-4' />
-    <div className='collapse-title text-xl font-medium'>Educational</div>
-    <div className='collapse-content'>
-      <p>hello</p>
-    </div>
-  </div>
-  <div className='collapse collapse-arrow join-item border border-base-300'>
-    <input type='radio' name='my-accordion-4' />
-    <div className='collapse-title text-xl font-medium'>Professional</div>
-    <div className='collapse-content'>
-      <p>hello</p>
-    </div>
-  </div>
-</div>;
+export const Accordian = () => {
+  return (
+    <>
+      <div className='join join-vertical w-full'>
+        <div className='collapse collapse-arrow join-item border border-base-300'>
+          <input type='radio' name='my-accordion-4' defaultChecked />
+          <div className='collapse-title text-xl font-medium'>Interests</div>
+          <div className='collapse-content'>
+            <p>hello</p>
+          </div>
+        </div>
+        <div className='collapse collapse-arrow join-item border border-base-300'>
+          <input type='radio' name='my-accordion-4' />
+          <div className='collapse-title text-xl font-medium'>Educational</div>
+          <div className='collapse-content'>
+            <p>hello</p>
+          </div>
+        </div>
+        <div className='collapse collapse-arrow join-item border border-base-300'>
+          <input type='radio' name='my-accordion-4' />
+          <div className='collapse-title text-xl font-medium'>Professional</div>
+          <div className='collapse-content'>
+            <p>hello</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
